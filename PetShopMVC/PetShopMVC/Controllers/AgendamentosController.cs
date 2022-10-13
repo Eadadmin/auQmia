f80246e1e27f22f25c1d4b687af3dba2d4a70f8e
raquell.souza@aluno.faculdadeimpacta.com.br <raquell.souza@aluno.faculdadeimpacta.com.br>
2022-10-13 17:35:45 -0300

    Implementando Pesquisa Parte 1

--- PetShopMVC/PetShopMVC/Controllers/AgendamentosController.cs
+++ PetShopMVC/PetShopMVC/Controllers/AgendamentosController.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetShopMVC.Services;
 
 namespace PetShopMVC.Controllers
 {
     public class AgendamentosController : Controller
     {
+        private readonly AgendamentoService _agendamentoService;
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
+            var result = await _agendamentoService.FindByDateAsync(minDate, maxDate);
             return View();
         }
 
---
+++ PetShopMVC/PetShopMVC/Services/AgendamentoService.cs
@@ -0,0 +1,42 @@
+﻿using PetShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PetShopMVC.Services
+{
+    public class AgendamentoService
+    {
+        private readonly PetShopMVCContext _context;
+
+        public AgendamentoService(PetShopMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Agendamento>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.Agendamento select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= maxDate.Value);
+
+            }
+
+            return await result
+                .Include(x => x.Cliente)
+                .Include(x => x.Cliente.Servico)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+}
